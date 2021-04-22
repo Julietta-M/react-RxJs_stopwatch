091b4dbb1aa12040efd821a09c8ccaf09a1c34af
Julia <mjulia1794@gmail.com>
2021-04-22 14:53:34 +0300

    stopwatch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-const goodsFromServer = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
-];
+import { interval, Subject } from 'rxjs';
+import { takeUntil, buffer, map, filter, debounceTime } from 'rxjs/operators';
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+function App() {
+  const [time, setTime] = useState(0);
+  const [state, setState] = useState('stop');
+
+  const click$ = new Subject();
+
+  useEffect(() => {
+    const doubleClick$ = click$.pipe(
+      buffer(
+        click$.pipe(debounceTime(300)),
+      ),
+      map(list => list.length),
+      filter(x => x === 2),
+    );
+
+    const timer$ = new Subject();
+
+    interval(1000)
+      .pipe(takeUntil(timer$))
+      .subscribe(() => {
+        if (state === 'start') {
+          setTime(val => val + 1);
+        }
+      });
+
+    return () => {
+      timer$.next();
+      timer$.complete();
+    };
+  }, [state, click$]);
+
+  const start = () => {
+    setState('start');
+  };
+
+  const stop = () => {
+    setState('stop');
+    setTime(0);
+  };
+
+  const reset = () => {
+    setTime(0);
+  };
+
+  const wait = () => {
+    click$.next();
+    setState('wait');
+    click$.next();
+  };
+
+  const formatTime = () => {
+    const getSeconds = `0${(time % 60)}`.slice(-2);
+    const minutes = `${Math.floor(time / 60)}`;
+    const getMinutes = `0${minutes % 60}`.slice(-2);
+    const getHours = `0${Math.floor(time / 3600)}`.slice(-2);
+
+    return `${getHours} : ${getMinutes} : ${getSeconds}`;
+  };
+
+  return (
+    <div className="container shadow-sm p-3 mb-5 bg-body rounded">
+      <div className="date">
+        {formatTime()}
+      </div>
+      <button
+        className="btn btn-success"
+        type="button"
+        onClick={start}
+      >
+        Start
+      </button>
+      <button
+        className="btn btn-danger"
+        type="button"
+        onClick={stop}
+      >
+        Stop
+      </button>
+      <button
+        className="btn btn-primary"
+        type="button"
+        onClick={reset}
+      >
+        Reset
+      </button>
+      <button
+        className="btn btn-warning"
+        type="button"
+        onClick={wait}
+      >
+        Wait
+      </button>
+    </div>
+  );
+}
 
 export default App;
